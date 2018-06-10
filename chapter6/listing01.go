ea1ab426b10a3c3a948f38c83b938baa8eea5d93
yanxi zhu <547110356@qq.com>
2018-06-10 08:47:08 +0800

    Initial commit

---
+++ chapter6/listing01.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	// 分配一个逻辑处理器给调度器使用
+	runtime.GOMAXPROCS(1)
+
+	// wg用来等待程序完成
+	// 计数加2，表示要等待两个goroutine
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	fmt.Println("Start Goroutines")
+
+	go func() {
+		//defer wg.Done()
+		//time.Sleep(time.Second)
+		fmt.Println("\nyet another goroutine")
+	}()
+	// 声明一个匿名函数，并创建一个goroutine
+	go func() {
+		// 函数在退出时调用Done来通知main函数工作已经完成
+		defer wg.Done()
+		for count := 0; count < 3; count++ {
+			for char := 'a'; char < 'a'+26; char++ {
+				fmt.Printf("%c ", char)
+			}
+		}
+	}()
+
+	// 声明一个匿名函数，并创建一个goroutine
+	go func() {
+		defer wg.Done()
+		for count := 0; count < 3; count++ {
+			for char := 'A'; char < 'A'+26; char++ {
+				fmt.Printf("%c ", char)
+			}
+		}
+	}()
+
+	// 等待goroutine结束
+	fmt.Println("Waiting To Finish")
+	wg.Wait()
+
+	fmt.Println("\nTerminating Program")
+}
