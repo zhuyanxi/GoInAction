338454a22b206c1f96a726ea4abd34e964ae9219
yanxi zhu <547110356@qq.com>
2018-06-15 22:56:14 +0800

    add c7

---
+++ chapter7/list7-13.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/goinaction/code/chapter7/patterns/runner"
+)
+
+const timeout = 3 * time.Second
+
+func L713() {
+	log.Println("Starting work.")
+
+	// 为本次执行分配超时时间
+	r := runner.New(timeout)
+
+	// 加入要执行的任务
+	r.Add(createTask(), createTask(), createTask())
+
+	// 执行任务并处理结果
+	if err := r.Start(); err != nil {
+		switch err {
+		case runner.ErrTimeout:
+			log.Println("Terminating due to timeout.")
+			os.Exit(1)
+		case runner.ErrInterrupt:
+			log.Println("Terminating due to interrupt.")
+			os.Exit(2)
+		}
+	}
+
+	log.Println("Process ended.")
+}
+
+func createTask() func(int) {
+	return func(id int) {
+		log.Printf("Processor - Task #%d.", id)
+		time.Sleep(time.Duration(id) * time.Second)
+	}
+}
