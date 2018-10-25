39147f36bd47f032bc469b113f87e472069fe05b
yanxi zhu <547110356@qq.com>
2018-10-25 23:55:46 +0800

    Update BasicGoroutine/myroutine.go
    add decision whether if the ch is empty

--- BasicGoroutine/myroutine.go
+++ BasicGoroutine/myroutine.go
@@ -12,7 +12,10 @@ type myMsg struct {
 
 func main() {
 	fmt.Println("Go channels starting")
+	Test1()
+}
 
+func Test1() {
 	ch1 := make(chan *myMsg)
 	go sendChan(ch1, "channel 1")
 
@@ -22,20 +25,38 @@ func main() {
 	// for msg := range ch {
 	// 	fmt.Println("Msg ", msg.seqNum, ":", msg.message)
 	// }
-
+	ch1closed := false
+	ch2closed := false
 	for {
 		select {
-		case msg := <-ch1:
-			fmt.Println("channel 1:", msg.seqNum, " : ", msg.message)
-		case msg := <-ch2:
-			fmt.Println("channel 2:", msg.seqNum, " : ", msg.message)
+		case msg, ok := <-ch1:
+			if ok {
+				fmt.Println("channel 1:", msg.seqNum, " : ", msg.message)
+			} else {
+				if !ch1closed {
+					fmt.Println("ch1 closed")
+					ch1closed = true
+				}
+			}
+		case msg, ok := <-ch2:
+			if ok {
+				fmt.Println("channel 2:", msg.seqNum, " : ", msg.message)
+			} else {
+				if !ch2closed {
+					fmt.Println("ch2 closed")
+					ch2closed = true
+				}
+			}
+		}
+		if ch1closed && ch2closed {
+			break
 		}
 	}
 }
 
 func sendChan(out chan<- *myMsg, prefix string) {
 	n := 0
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 10; i++ {
 		time.Sleep(time.Second)
 		out <- &myMsg{n, fmt.Sprintf("%s: %s", prefix, "moo")}
 		n++
