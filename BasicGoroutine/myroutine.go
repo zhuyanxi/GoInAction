93130b2f55f6609625932bdf46396f0341b5ad58
yanxi zhu <547110356@qq.com>
2018-10-25 22:55:19 +0800

    Update BasicGoroutine/myroutine.go
    change one chan value to two chan values ,but  still will halt

--- BasicGoroutine/myroutine.go
+++ BasicGoroutine/myroutine.go
@@ -13,19 +13,32 @@ type myMsg struct {
 func main() {
 	fmt.Println("Go channels starting")
 
-	ch := make(chan *myMsg)
-	go sendChan(ch, "channel 1")
-	go sendChan(ch, "channel 2")
+	ch1 := make(chan *myMsg)
+	go sendChan(ch1, "channel 1")
 
-	for msg := range ch {
-		fmt.Println("Msg ", msg.seqNum, ":", msg.message)
+	ch2 := make(chan *myMsg)
+	go sendChan(ch2, "channel 2")
+
+	// for msg := range ch {
+	// 	fmt.Println("Msg ", msg.seqNum, ":", msg.message)
+	// }
+
+	for {
+		select {
+		case msg := <-ch1:
+			fmt.Println("channel 1:", msg.seqNum, " : ", msg.message)
+		case msg := <-ch2:
+			fmt.Println("channel 2:", msg.seqNum, " : ", msg.message)
+		}
 	}
 }
 
 func sendChan(out chan<- *myMsg, prefix string) {
+	n := 0
 	for i := 0; i < 5; i++ {
 		time.Sleep(time.Second)
-		out <- &myMsg{i, fmt.Sprintf("%s: %s", prefix, "moo")}
+		out <- &myMsg{n, fmt.Sprintf("%s: %s", prefix, "moo")}
+		n++
 	}
 	close(out)
 }
